998c9c4988253f2c5bf360d2bc3433d63558cd31
Ed Perry <edwardperry1@gmail.com>
2016-06-12 13:50:45 -0400

    Added comments and fixed indention

--- esTail.js
+++ esTail.js
@@ -25,11 +25,6 @@ var regexflags="gm";
 var rawoutput;
 // Disable Info messages
 console.info = function (){};
-/**************************************************
-**
-** Varables
-**
-***************************************************/
 // Count of documents retrieved which tells us when the scan/scroll is finished
 // flag when the search scan/scoll is retrieved
 var searchDone=true;
@@ -49,227 +44,238 @@ var context = {
     from:"now-10m",
     fetchsize: 100
 }
-function printOutput(){
-  //s.sort(function ( a, b){
-//	  a1 = moment(a._source["@timestamp"],"YYYY-MM-DDTHH:mm:ss.SSSZ").format("x");
-//	  b1 = moment(b._source["@timestamp"],"YYYY-MM-DDTHH:mm:ss.SSSZ").format("x");
-//	  console.log(a1-b1);
-//	  return a1-b1;
-//	});
-	console.info("INFO".yellow+" inPrintOutput length to print="+output.length); 
-	while ( output.length > 0 ) {
-		hit = output.shift();	
-		console.info("===="+hit+" of "+output.length);
-		// If allfields cli option is set show all the fields not just one field
-		if ( allfields ) {
-			console.log(hit._source["@timestamp"].red+":\n".green+JSON.stringify(hit._source));
+/****************************************************************
+*** need to convert to call back function
+*****************************************************************/
+	function printOutput(){
+	  //s.sort(function ( a, b){
+	//	  a1 = moment(a._source["@timestamp"],"YYYY-MM-DDTHH:mm:ss.SSSZ").format("x");
+	//	  b1 = moment(b._source["@timestamp"],"YYYY-MM-DDTHH:mm:ss.SSSZ").format("x");
+	//	  console.log(a1-b1);
+	//	  return a1-b1;
+	//	});
+		console.info("INFO".yellow+" inPrintOutput length to print="+output.length); 
+		while ( output.length > 0 ) {
+			hit = output.shift();	
+			console.info("===="+hit+" of "+output.length);
+			// If allfields cli option is set show all the fields not just one field
+			if ( allfields ) {
+				console.log(hit._source["@timestamp"].red+":\n".green+JSON.stringify(hit._source));
 
-		}else{
-			// If not allfields 
-			// If rawoutput is set Pretty Print the json as output
-			if (rawoutput) {
-				console.log(JSON.stringify(hit,null,2));
 			}else{
-				//If not rawoutput print <indexed time>: <index>:message
-				console.log(hit._source["@timestamp"].red+": ".green+hit._index.green+":".green+hit._source.message)
-			}
+				// If not allfields 
+				// If rawoutput is set Pretty Print the json as output
+				if (rawoutput) {
+					console.log(JSON.stringify(hit,null,2));
+				}else{
+					//If not rawoutput print <indexed time>: <index>:message
+					console.log(hit._source["@timestamp"].red
+					+": ".green+hit._index.green+":".green
+					+hit._source.message)
+				}
 
-		}
-		// If set process the message field via the regex and print the results
-		if ( regex ) {
-			var result = hit._source.message.match(regex);
-			if ( result  ){
-				console.log("\tregex: ".red+JSON.stringify(result).yellow);	
 			}
-		}
-		// Set the time of the last message timestamp retrieved so we don't requery the same message
-		context.from = hit._source["@timestamp"];
-	  };
-}
+			// If set process the message field via the regex and print the results
+			if ( regex ) {
+				var result = hit._source.message.match(regex);
+				if ( result  ){
+					console.log("\tregex: ".red+JSON.stringify(result).yellow);	
+				}
+			}
+			// Set the time of the last message timestamp retrieved so we don't requery the same message
+			context.from = hit._source["@timestamp"];
+		  };
+	}
 /**************************************************
 **
 ** Do Search
 **
 ***************************************************/
-function doSearch(client,searchTemplate){
-	console.info("Running search".blue);
-        if (! searchDone ) {
-		console.log("Search Not Complete");
-		return;
+	function doSearch(client,searchTemplate){
+		console.info("Running search".blue);
+		if (! searchDone ) {
+			console.log("Search Not Complete");
+			return;
+		}
+		// convert the Template to a valid search
+		var search = markupjs.up(searchTemplate,context); 
+		// Execute the Search
+		client.search( JSON.parse(search) , ph = function printHits(error, response) {
+		  // Loop over the events
+		  if (error != undefined) {
+			console.error("ERR:".red+error);
+			return;
+		  }
+		  console.info("INFO".yellow+"Count = "+response.hits.hits.length);
+		  response.hits.hits.forEach(function (hit) {
+			// If allfields cli option is set show all the fields not just one field
+			//console.info("INFO".yellow+"Push Object");
+			output.push(hit);
+		  });
+		  // If the retrieved docements equals the count then we are done
+		  printOutput()
+		  if ( output.length >= response.hits.total ){ 
+			  searchDone=true;
+			  console.info("Search complete".blue)
+			  return;
+		  }
+		  // Else query the scroll again to get more documents
+		  client.scroll({
+		      scrollId: response._scroll_id,
+		      scroll: '30s'
+		  }, ph);
+		});
 	}
-	// convert the Template to a valid search
-	var search = markupjs.up(searchTemplate,context); 
-	// Execute the Search
-	client.search( JSON.parse(search) , ph = function printHits(error, response) {
-	  // Loop over the events
-	  if (error != undefined) {
-		console.error("ERR:".red+error);
-		return;
-	  }
-	  console.info("INFO".yellow+"Count = "+response.hits.hits.length);
-	  response.hits.hits.forEach(function (hit) {
-		// If allfields cli option is set show all the fields not just one field
-		//console.info("INFO".yellow+"Push Object");
-                output.push(hit);
-	  });
-	  // If the retrieved docements equals the count then we are done
-	  printOutput()
-	  if ( output.length >= response.hits.total ){ 
-		  searchDone=true;
-		  console.info("Search complete".blue)
-		  return;
-	  }
-	  // Else query the scroll again to get more documents
-	  client.scroll({
-	      scrollId: response._scroll_id,
-	      scroll: '30s'
-	  }, ph);
-	});
-}
+/**********************************************************************
+*** Initialize function
+***********************************************************************
 module.exports = {
-init: function() {
-	/***************************************************
-	**
-	** Setup
-	**
-	***************************************************/
-	/*******************************
-	**
-	** Process Command Line 
-	**
-	********************************/
-	console.info("Processing Commandline arguments");
-	process.argv.forEach(function (val, ind, array) {
-	    if(/^(-h|--help|-\?)$/.test(val) ){
-		console.log(process.argv[0]+":");
-		console.log("\t[--hostport="+hostportlist+"]");
-		console.log("\t[--search=<filename> default: "+searchFilename);
-		console.log("\t[--regex='([\d\.]+)' default: none");
-		console.log("\t[--regexflags='gm'   default: "+regexflags);
-		console.log("\t[--allfields         default: false ");
-		console.log("\t[--raw         	    default: false ");
-		console.log("\t[--fetchsize='20'  default: 100 ");
-		console.log("\t[-i|--refreshInterval='1000'  default: "+refreshInterval);
-		console.log("\t\t\tHow often a new search is issued");
-		console.log("\t[--context='{ 'custom':'json'}'  default:"+JSON.stringify(context) );
-		console.log("\t\t\tContext is what varables pass to the search template for json markup");
-		console.log("\t\t\tcontext=<key>=<val> is a way to set any varable inside the context array. Make sure this is used after --contextfile or --context=<customejson>");
-		console.log("\t[--index=<index>|--context=index=<index>     default: "+context.index);
-		console.log("\t[--from=<datestamp>|--context=from='now-5m'  default: "+context.from);
-		console.log("\t\t\tfrom can be of any valid Elasticsearch timevalue or Caclulation ");
-		process.exit(1)
-	    }
-	    if (val === "--allfields" ){
-		    allfields = true;
-		    console.info("--allfields="+allfields);
-	    }
-	    if (val === "--raw"){
-		    rawoutput=true;
-		    console.info("--raw="+rawoutput);
-	    }
-	    if(val.indexOf('=') >0){
-		var s = val.split(/=/);
-		console.info(s[0] + ' : ' + s[1]); 
-		if (s[0] === "--hostport" ){
-		    hostportlist=s[1];
-		}
-		if (s[0] === "--regexflags" ){
-		    regexflags =  s[1];
-		}
-		if (s[0] === "--regex" ){
-		    regex = s[1];
-		}
-		if (s[0] === "--loglevel" ){
-		    loglevel = s[1];
-		}
-		if (s[0] === "--refreshinterval" || s[0] === "-i" ){
-		    refreshInterval = s[1];
-		}
-		if (s[0] === "--contextfile" ){
-		    context = s[1];
-		    if (fs.existsSync(s[1])) {
-				searchTemplate = fs.readFileSync(s[1],'utf8'); 
-				console.info(searchTemplate);
-		    }else{
-				console.error("file does not exist:"+s[1]);
-				process.exit(2);
+	init: function() {
+		/***************************************************
+		**
+		** Setup
+		**
+		***************************************************/
+		/*******************************
+		**
+		** Process Command Line 
+		**
+		********************************/
+		console.info("Processing Commandline arguments");
+		process.argv.forEach(function (val, ind, array) {
+		    if(/^(-h|--help|-\?)$/.test(val) ){
+			console.log(process.argv[0]+":");
+			console.log("\t[--hostport="+hostportlist+"]");
+			console.log("\t[--search=<filename> default: "+searchFilename);
+			console.log("\t[--regex='([\d\.]+)' default: none");
+			console.log("\t[--regexflags='gm'   default: "+regexflags);
+			console.log("\t[--allfields         default: false ");
+			console.log("\t[--raw         	    default: false ");
+			console.log("\t[--fetchsize='20'  default: 100 ");
+			console.log("\t[-i|--refreshInterval='1000'  default: "+refreshInterval);
+			console.log("\t\t\tHow often a new search is issued");
+			console.log("\t[--context='{ 'custom':'json'}'  default:"+JSON.stringify(context) );
+			console.log("\t\t\tContext is what varables pass to the search template for json markup");
+			console.log("\t\t\tcontext=<key>=<val> is a way to set any varable inside the context array. Make sure this is used after --contextfile or --context=<customejson>");
+			console.log("\t[--index=<index>|--context=index=<index>     default: "+context.index);
+			console.log("\t[--from=<datestamp>|--context=from='now-5m'  default: "+context.from);
+			console.log("\t\t\tfrom can be of any valid Elasticsearch timevalue or Caclulation ");
+			process.exit(1)
 		    }
-		    context = JSON.parse(context);
+		    if (val === "--allfields" ){
+			    allfields = true;
+			    console.info("--allfields="+allfields);
+		    }
+		    if (val === "--raw"){
+			    rawoutput=true;
+			    console.info("--raw="+rawoutput);
+		    }
+		    if(val.indexOf('=') >0){
+			var s = val.split(/=/);
+			console.info(s[0] + ' : ' + s[1]); 
+			if (s[0] === "--hostport" ){
+			    hostportlist=s[1];
+			}
+			if (s[0] === "--regexflags" ){
+			    regexflags =  s[1];
+			}
+			if (s[0] === "--regex" ){
+			    regex = s[1];
+			}
+			if (s[0] === "--loglevel" ){
+			    loglevel = s[1];
+			}
+			if (s[0] === "--refreshinterval" || s[0] === "-i" ){
+			    refreshInterval = s[1];
+			}
+			if (s[0] === "--contextfile" ){
+			    context = s[1];
+			    if (fs.existsSync(s[1])) {
+					searchTemplate = fs.readFileSync(s[1],'utf8'); 
+					console.info(searchTemplate);
+			    }else{
+					console.error("file does not exist:"+s[1]);
+					process.exit(2);
+			    }
+			    context = JSON.parse(context);
+			}
+			if (s[0] === "--context" && s.length == 2){
+			    context = s[1];
+			    context = JSON.parse(context);
+			}
+			if (s[0] === "--context" && s.length > 2 ){
+			    console.log(s);
+			    context[s[1]]=s[2]
+			    console.info("context."+s[1]+"="+s[2]);
+			}
+			if (s[0] === "--search"){
+			    searchFilename=s[1];
+			}
+			if (s[0] === "--index"){
+			    context.index=s[1];
+			}
+		    }
+		});
+	},
+	/***********************************************************************
+	*** Main Module section
+	************************************************************************/
+	estail: function() {
+		// Convert CLI options to an actual regex expression and set the regex output to be displayed
+		regex = new RegExp( regex,regexflags);
+		// Load the defaultSearch
+		if (fs.existsSync(searchFilename)) {
+			var searchTemplate = fs.readFileSync(searchFilename,'utf8'); 
+			//console.info(searchTemplate);
+		}else{
+			console.error("file does not exist:"+searchFilename);
+			process.exit(2);
 		}
-		if (s[0] === "--context" && s.length == 2){
-		    context = s[1];
-		    context = JSON.parse(context);
-		}
-		if (s[0] === "--context" && s.length > 2 ){
-		    console.log(s);
-		    context[s[1]]=s[2]
-		    console.info("context."+s[1]+"="+s[2]);
-		}
-		if (s[0] === "--search"){
-		    searchFilename=s[1];
-		}
-		if (s[0] === "--index"){
-		    context.index=s[1];
-		}
-	    }
-	});
-},
-estail: function() {
-// Convert CLI options to an actual regex expression and set the regex output to be displayed
-regex = new RegExp( regex,regexflags);
-// Load the defaultSearch
-if (fs.existsSync(searchFilename)) {
-	var searchTemplate = fs.readFileSync(searchFilename,'utf8'); 
-	//console.info(searchTemplate);
-}else{
-	console.error("file does not exist:"+searchFilename);
-	process.exit(2);
-}
-// Open the Elasticsearch Connection
-client = new elasticsearch.Client({
-  host: hostportlist,
-  sniffOnStart: true,
-  sniffInterval: 60000,
-  index: context.index,
-  keepAlive: true ,
-  ignore: [404],
-  log: loglevel,
-  suggestCompression: true,
-  sniffOnStart: true,
-  sniffInterval: 60000
-});
-/**************************************************
-**
-** Test Connection make sure it is available
-**
-***************************************************/
-client.ping({
-  requestTimeout: 1000,
-}, function (error) {
-  if (error) {
-    console.error('elasticsearch cluster maybe down!');
-    process.exit(1);
-  }else{
-    console.log('Connected to Elasticsearch cluster.')	
-  } 
-});
+		// Open the Elasticsearch Connection
+		client = new elasticsearch.Client({
+		  host: hostportlist,
+		  sniffOnStart: true,
+		  sniffInterval: 60000,
+		  index: context.index,
+		  keepAlive: true ,
+		  ignore: [404],
+		  log: loglevel,
+		  suggestCompression: true,
+		  sniffOnStart: true,
+		  sniffInterval: 60000
+		});
+		/**************************************************
+		**
+		** Test Connection make sure it is available
+		**
+		***************************************************/
+		client.ping({
+		  requestTimeout: 1000,
+		}, function (error) {
+		  if (error) {
+		    console.error('elasticsearch cluster maybe down!');
+		    process.exit(1);
+		  }else{
+		    console.log('Connected to Elasticsearch cluster.')	
+		  } 
+		});
 
-/********************************************************************************
-**
-** Functions
-**
-*********************************************************************************/
-// Main search
-/********************************************************************************
-**
-** Application
-**
-*********************************************************************************/
-// set the loop for retrieving files
-setInterval ( 
-	function () { 
-	if(searchDone) { 
-		doSearch(client,searchTemplate)
-	}}, refreshInterval );
-}
+		/********************************************************************************
+		**
+		** Functions
+		**
+		*********************************************************************************/
+		// Main search
+		/********************************************************************************
+		**
+		** Application
+		**
+		*********************************************************************************/
+		// set the loop for retrieving files
+		setInterval ( 
+			function () { 
+			if(searchDone) { 
+				doSearch(client,searchTemplate)
+			}}, refreshInterval );
+	}
 }
